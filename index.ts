c26339b02662e6d5076a92b673be669b37e4e722
Paul Melinte <paul.melinte@softwire.com>
2021-06-29 13:27:17 +0300

    Print Fizz when divisible by 3

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 function fizzBuzz(n: number) {
     for(let i = 1;i <= n;i++){
-        console.log(i);
+        if(i % 3 == 0)
+            console.log("Fizz");
+        else console.log(i);
     }
 }
 
