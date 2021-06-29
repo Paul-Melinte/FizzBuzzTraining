3f3af8e53931213d2e55f524831edf8a295880c9
Paul Melinte <paul.melinte@softwire.com>
2021-06-29 13:30:18 +0300

    Printing Buzz when divisible by 5 Saving the parts of the output in a array of string.

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
 function fizzBuzz(n: number) {
     for(let i = 1;i <= n;i++){
+        const strings = []; // Using a array of strings to save the different parts of the answer
+        //Decided to do this because we can't change a const and I didn't want to keep track of which string we have to print at the end
+        
         if(i % 3 == 0)
-            console.log("Fizz");
-        else console.log(i);
+            strings.push("Fizz");
+
+        if(i % 5 == 0)
+            strings.push("Buzz");
+
+        if(strings.length == 0)
+            console.log(i);
+        else console.log(strings.join(""));
+
     }
 }
 
